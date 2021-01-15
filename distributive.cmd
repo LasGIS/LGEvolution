52df918a2c3c29b815c84603b375b9d5681c2dfd
LasGIS <lasgis@gmail.com>
2021-01-15 19:28:46 +0600

    LGEvolution: сделал дистрибутив

--- distributive.cmd
+++ distributive.cmd
@@ -1,6 +1,6 @@
-::call mvn clean install -P maximum
+call mvn clean install -P maximum
 cd Evolution
-::call mvn assembly:single
+call mvn assembly:single
 cd ../
-md dist
+::md dist
 copy Evolution\target\evolution-1.3.2-SNAPSHOT-jar-with-dependencies.jar dist\evolution.jar
